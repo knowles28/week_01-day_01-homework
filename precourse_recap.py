2ecf337bd4e990d7a298a9adde9c472511f2cab8
Jack Knowles <jackmhknowles@gmail.com>
2022-06-20 22:12:28 +0100

    second commit

--- precourse_recap.py
+++ precourse_recap.py
@@ -1,8 +1,18 @@
-team_1 = "Leeds"
-team_2 = "Liverpool"
-points_team_1 = 39
-points_team_2 = 90
+from cgi import test
 
-score = points_team_1 + points_team_2
 
-print(score)
+t1_goalsfor = 99
+t2_goalsfor = 94
+t3_goalsfor = 76
+
+t1_goalsagainst = 26
+t2_goalsagainst = 26
+t3_goalsagainst = 33
+
+t1_gd = t1_goalsfor - t1_goalsagainst
+t2_gd = t2_goalsfor - t2_goalsagainst
+t3_gd = t3_goalsfor - t3_goalsagainst
+
+average_gd = (t1_gd + t2_gd + t3_gd) / 3
+
+print(average_gd)
